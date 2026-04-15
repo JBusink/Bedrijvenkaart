49a14d5c4ec91c8dc06649286110c945b016ac40
Joris Busink <94122040+JBusink@users.noreply.github.com>
2026-04-15 12:54:50 +0200

    Initial commit

---
+++ js/mais.js
@@ -0,0 +1,31 @@
+// Kaart initialiseren
+const map = L.map('map').setView([52.2, 5.3], 7);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; OpenStreetMap'
+}).addTo(map);
+
+// Data inladen
+fetch('data/bedrijven.json')
+  .then(res => res.json())
+  .then(bedrijven => {
+
+    const markers = [];
+
+    bedrijven.forEach(bedrijf => {
+      const marker = L.marker([bedrijf.lat, bedrijf.lng])
+        .addTo(map)
+        .bindPopup(`
+          <b>${bedrijf.naam}</b><br>
+          ${bedrijf.plaats}<br>
+          ${bedrijf.beschrijving}
+        `);
+
+      markers.push(marker);
+    });
+
+    // Zoom op alles
+    const groep = L.featureGroup(markers);
+    map.fitBounds(groep.getBounds().pad(0.2));
+  });
