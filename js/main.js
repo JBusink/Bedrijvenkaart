7d1000d305f62e22ed71d51e61e199022015f2a1
Joris Busink <94122040+JBusink@users.noreply.github.com>
2026-04-15 13:32:04 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
-// Kaart initialiseren (Nederland)
 const map = L.map('map').setView([52.2, 5.3], 7);
 
-// Kaartlaag (OpenStreetMap)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; OpenStreetMap'
 }).addTo(map);
 
-// Test marker
-L.marker([52.37, 4.90])
-  .addTo(map)
-  .bindPopup("Amsterdam")
-  .openPopup();
+fetch('data/bedrijven.json')
+  .then(res => res.json())
+  .then(bedrijven => {
+    const markers = [];
+
+    bedrijven.forEach(bedrijf => {
+      const marker = L.marker([bedrijf.lat, bedrijf.lng])
+        .addTo(map)
+        .bindPopup(`
+          <b>${bedrijf.naam}</b><br>
+          ${bedrijf.plaats}<br>
+          ${bedrijf.beschrijving}
+        `);
+
+      markers.push(marker);
+    });
+
+    if (markers.length > 0) {
+      const groep = L.featureGroup(markers);
+      map.fitBounds(groep.getBounds().pad(0.2));
+    }
+  });
