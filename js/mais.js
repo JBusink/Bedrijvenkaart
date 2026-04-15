c31fd585cf265161b141a7f67a9aa0defa397d12
Joris Busink <94122040+JBusink@users.noreply.github.com>
2026-04-15 13:10:11 +0200

    Update mais.js

--- js/mais.js
+++ js/mais.js
@@ -6,7 +6,12 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 fetch('data/bedrijven.json')
-  .then(res => res.json())
+  .then(res => {
+    if (!res.ok) {
+      throw new Error('JSON niet gevonden');
+    }
+    return res.json();
+  })
   .then(bedrijven => {
 
     const markers = [];
@@ -23,7 +28,11 @@ fetch('data/bedrijven.json')
       markers.push(marker);
     });
 
-    // Zoom op alles
-    const groep = L.featureGroup(markers);
-    map.fitBounds(groep.getBounds().pad(0.2));
+    if (markers.length > 0) {
+      const groep = L.featureGroup(markers);
+      map.fitBounds(groep.getBounds().pad(0.2));
+    }
+  })
+  .catch(err => {
+    console.error('Fout:', err);
   });
