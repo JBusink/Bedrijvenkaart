6eb988aeb0e04c0175f172fbe815eef943403910
Joris Busink <94122040+JBusink@users.noreply.github.com>
2026-04-15 13:26:59 +0200

    Update mais.js

--- js/mais.js
+++ js/mais.js
@@ -1,44 +1,14 @@
-console.log("main.js geladen");
-
+// Kaart initialiseren (Nederland)
 const map = L.map('map').setView([52.2, 5.3], 7);
 
+// Kaartlaag (OpenStreetMap)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; OpenStreetMap'
 }).addTo(map);
 
-fetch('data/bedrijven.json')
-  .then(res => {
-    console.log("fetch response:", res.status, res.url);
-    if (!res.ok) {
-      throw new Error(`Kon bedrijven.json niet laden: ${res.status}`);
-    }
-    return res.json();
-  })
-  .then(bedrijven => {
-    console.log("bedrijven geladen:", bedrijven);
-
-    const markers = [];
-
-    bedrijven.forEach(bedrijf => {
-      const marker = L.marker([bedrijf.lat, bedrijf.lng])
-        .addTo(map)
-        .bindPopup(`
-          <b>${bedrijf.naam}</b><br>
-          ${bedrijf.plaats}<br>
-          ${bedrijf.beschrijving}
-        `);
-
-      markers.push(marker);
-    });
-
-    if (markers.length > 0) {
-      const groep = L.featureGroup(markers);
-      map.fitBounds(groep.getBounds().pad(0.2));
-    } else {
-      console.warn("Geen markers gevonden in bedrijven.json");
-    }
-  })
-  .catch(err => {
-    console.error("Fout in main.js:", err);
-  });
+// Test marker
+L.marker([52.37, 4.90])
+  .addTo(map)
+  .bindPopup("Amsterdam")
+  .openPopup();
